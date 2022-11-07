355db4204086cb9a9867dc28b54e3d32b42ed5aa
Rszooc <cursoszudemy@gmail.com>
2022-11-07 10:55:28 -0600

    Ajuste de iconos

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -7,7 +7,7 @@
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         {{-- <x-jet-application-mark class="block h-9 w-auto" /> --}}
-                        <img src="img/PJ.png" alt="" class="w-10">
+                        <img src="/img/PJ.png" alt="" class="w-10">
                     </a>
                 </div>
 
